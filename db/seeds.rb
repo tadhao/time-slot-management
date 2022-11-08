16c28c8193d0b107485a6999f6371f3460e2a45c
tadhao <tadhao@gmail.com>
2022-11-08 18:00:19 +0530

    Added demo data in seed file for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+date = DateTime.now + 1
+start_time = DateTime.parse(date.strftime('%Y-%m-%dT01:00:00%z'))
+end_time = DateTime.parse(date.strftime('%Y-%m-%dT02:00:00%z'))
+Slot.create([{ start_time: start_time, end_time: end_time, total_capacity: 6 }])
